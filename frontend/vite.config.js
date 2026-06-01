6511ff403103e0a4badf1ceb2b555b5e7e63484c
Ass-Gaye <assgaye83@gmail.com>
2026-06-01 17:46:45 +0000

    Fix Tailwind Vite import

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailWindcss from '@tailWindcss/Vite'
+import tailWindcss from '@tailWindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
